0234b29f4dea36f5832ae480ddc278ca46dff747
Dejing <dejingk@gmail.com>
2021-09-28 20:32:26 -0400

    task: turn off debug model

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)bk&bn$dv*lpmbl-l8^5t&!0&bm4@vuef-9_k=-g+78q1uxvxn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
